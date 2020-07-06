60038112275c152626d2e8a4302a4a834c12e647
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-06 20:00:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,37 @@
 def new_hash
+  new_hash= Hash.new
+  new_hash
   # return an empty hash
 end
 
-def my_hash
-  # return a valid hash with any key/value pair of your choice
+def my_hash 
+  my_hash=Hash.new
+  my_hash ={
+    anime:"Sailor Moon",
+    cartoon:"American Dad",
+    tv_show:"What We Do In The Shadows"
+  }
 end
 
+
+
 def pioneer
+  pioneer=Hash.new
+  pioneer={
+    :name => "Grace Hopper"
+  }
+  pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id_hash_generator=Hash.new
+  if id_hash_generator[:id]
+    puts id_hash_generator=id_hash_generator[:id] +=number
+  else 
+      puts id_hash_generator[:id] = number 
+  end
+  puts id_hash_generator
 end
